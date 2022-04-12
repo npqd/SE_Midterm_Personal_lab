d05eb89320e6552c100fd5d74f6e9a1d86e8b6bc
npqd <quangdungreal1712@gmail.com>
2022-04-13 00:42:53 +0700

    URLS and Views (pt2) done

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('home', views.home),
+    path('', views.home),
     path('products/', views.products),
     path('customer/', views.customer),
 ]
