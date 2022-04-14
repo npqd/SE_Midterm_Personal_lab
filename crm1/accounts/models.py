4f43459a1a64583db953f20d0ad35cf6ade963ed
npqd <quangdungreal1712@gmail.com>
2022-04-14 10:54:58 +0700

    Database Models & Admin Panel (pt5) done

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,3 +1,38 @@
+import email
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add = True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Products(models.Model):
+    CATEGORY = (
+        ('In door','In door'),
+        ('Out door','Out door'),
+    )
+    
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    data_created = models.DateTimeField(auto_now_add = True, null=True)
+
+class Order(models.Model):
+
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Deliveried', 'Deliveried'),
+    )
+
+    #customer =
+    #product = 
+    date_created = models.DateTimeField(auto_now_add = True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
