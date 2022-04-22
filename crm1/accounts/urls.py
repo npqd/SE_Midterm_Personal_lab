a6ae5c9559be91055a52ecd33726cf8b74483619
npqd <quangdungreal1712@gmail.com>
2022-04-22 17:08:36 +0700

    Image File Upload (pt17) done

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('user/',views.userPage,name="user-page"),
     path('products/', views.products,name = "products"),
+    path('account/', views.accountSettings, name="account"),
 
     path('customer/<str:pk_test>/', views.customer,name = "customer"),
     path('create_order/<str:pk>/',views.createOrder,name ="create_order"),
